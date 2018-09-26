da2b162d4b5dfbc07f270bb69511c8b77760d7f4
Craig Peters <craig.l.peters@gmail.com>
2018-09-26 09:49:00 -0700

    removing all dependencies

--- app.go
+++ app.go
@@ -7,21 +7,21 @@ import (
 	"os"
 
 //	"github.com/ddo/rq"
-	"rsc.io/quote"
+//	"rsc.io/quote"
 )
 
 func main() {
 	c := os.Getenv("COLOR")
 //	r := rq.Get("https://httpbin.org/get")
 //	r.Qs("q", "1", "2")
-	q := quote.Hello()
+//	q := quote.Hello()
 	if len(c) == 0 {
 		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E.
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n '"+q+"' </html>", r.URL.Path)
-//		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n </html>", r.URL.Path)
+//		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n '"+q+"' </html>", r.URL.Path)
+		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n </html>", r.URL.Path)
 	})
 
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
