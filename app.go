3562ffb1ed553c4334e12e53e09ab7bd20d13163
Craig Peters <craig.l.peters@gmail.com>
2018-09-25 20:32:02 -0700

    Update app.go

--- app.go
+++ app.go
@@ -5,19 +5,19 @@ import (
 	"net/http"
 	"os"
 
-	"rsc.io/quote"
-	"github.com/bmizerany/pat"
+//	"rsc.io/quote"
 )
 
 func main() {
-	c := os.Getenv("COLOR")
+//	c := os.Getenv("COLOR")
 	q := quote.Hello()
 	if len(c) == 0 {
 		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E.
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n '"+q+"' </html>", r.URL.Path)
+//		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n '"+q+"' </html>", r.URL.Path)
+		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n </html>", r.URL.Path)
 	})
 
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
